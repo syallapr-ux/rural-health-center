0a61567ea4003a84e0c391d83149127c8c1fd191
syallapr-ux <syallapr@gitam.in>
2025-12-31 17:38:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,10 @@
-const CACHE_NAME = 'rural-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './data.json'
-];
+const CACHE_NAME = 'rh-cache-v1';
+const ASSETS = ['./', './index.html', './style.css', './script.js', './data.json'];
 
-// Install Event
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-    );
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-// Intercept Network Requests for Offline Use
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
